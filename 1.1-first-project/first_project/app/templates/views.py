c0a0ecbd2b5a4839f9aa046bf3233016740546a1
clonefuture <clonefuture@gmail.com>
2022-05-29 00:20:31 +1000

    1st

--- 1.1-first-project/first_project/app/templates/views.py
+++ 1.1-first-project/first_project/app/templates/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
@@ -23,7 +25,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
