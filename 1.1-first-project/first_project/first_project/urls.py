ba77641c4f221b7c10adc0f39235cd027752a0df
clonefuture <clonefuture@gmail.com>
2022-05-30 21:42:34 +1000

    correct

--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.templates.views import home_view, time_view, workdir_view
+from app.views import home_view, time_view, workdir_view
 
 urlpatterns = [
     path('', home_view, name='home'),
